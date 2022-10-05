67797b3a9ac6c327bb506b864f47f9055d03c247
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-05 17:58:10 +0200

    feat: sidebar links / tweet button / profile info done

--- twitter-clone/components/Sidebar.js
+++ twitter-clone/components/Sidebar.js
@@ -1,4 +1,17 @@
 import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
 
 export default function Sidebar() {
   return (
@@ -8,7 +21,34 @@ export default function Sidebar() {
         hoverAnimation p-0 xl:ml-24 ">
             <Image src="https://rb.gy/ogau5a" width={30} height={30}/>
         </div>
-        
+        <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+        </div>
+        <button className="hidden  xl:inline ml-auto bg-[#1d9bf0]
+         text-white rounded-full w-56 h-[52px] text-lg 
+         font-bold shadow-md hover:bg-[#1a8cd8]">
+            Tweet
+        </button>
+        <div
+        className="text-[#d9d9d9] flex items-center justify-center 
+        hoverAnimation xl:ml-auto xl:-mr-0 mt-auto "
+        >
+            <img src="" alt=""
+            className="h-10 w-10 rounded-full xl:mr-2.5" 
+            />
+            <div className="hidden xl:inline leading-5">
+                <h4 className="font-bold">Essabessa</h4>
+                <p className="text-[#6e767d]">@essabessa</p>
+            </div>
+            <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
+        </div>
     </div>
   )
 }
