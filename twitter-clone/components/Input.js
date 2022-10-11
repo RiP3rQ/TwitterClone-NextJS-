6477325c37fd7ad6aaf706f651c426a050131830
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-11 15:43:04 +0200

    feat: add img to post

--- twitter-clone/components/Input.js
+++ twitter-clone/components/Input.js
@@ -76,7 +76,8 @@ export default function Input() {
   
     return (
         <div
-        className={`border-b border-gray-700 p-3 flex space-x-3 scrollbar-hide `}
+        className={`border-b border-gray-700 p-3 flex space-x-3 scrollbar-hide 
+        ${loading && 'opacity-60'}`}
         >
             <img src="" alt="" className="h-11 w-11 rounded-full cursor-pointer" />
             <div className="w-full divide-y divide-gray-700">
@@ -106,58 +107,60 @@ export default function Input() {
                 </div>
             )}
         
+                {!loading && (
+                  <div className="flex items-center justify-between pt-2.5">
+                      <div className="flex items-center">
+                          <div className="icon"
+                          onClick={() => filePickerRef.current.click()}
+                          >
+                              <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
+                              <input 
+                              type="file" 
+                              hidden
+                              onChange={addImageToPost} 
+                              ref={filePickerRef}/>
+                          </div>
 
-                <div className="flex items-center justify-between pt-2.5">
-                    <div className="flex items-center">
-                        <div className="icon"
-                        onClick={() => filePickerRef.current.click()}
-                        >
-                            <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
-                            <input 
-                            type="file" 
-                            hidden
-                            onChange={addImageToPost} 
-                            ref={filePickerRef}/>
-                        </div>
+                          
+                      <div className="icon rotate-90">
+                        <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+                      </div>
 
-                        
-              <div className="icon rotate-90">
-                <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
+                      <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                        <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+                      </div>
 
-              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
-                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
+                      <div className="icon">
+                        <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+                      </div>
 
-              <div className="icon">
-                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
-              </div>
+                      {showEmojis && (
+                        <Picker
+                          onSelect={addEmoji}
+                          style={{
+                            position: "absolute",
+                            marginTop: "465px",
+                            marginLeft: -40,
+                            maxWidth: "320px",
+                            borderRadius: "20px",
+                          }}
+                          theme="dark"
+                        />
+                      )}
+                  </div>
 
-              {showEmojis && (
-                <Picker
-                  onSelect={addEmoji}
-                  style={{
-                    position: "absolute",
-                    marginTop: "465px",
-                    marginLeft: -40,
-                    maxWidth: "320px",
-                    borderRadius: "20px",
-                  }}
-                  theme="dark"
-                />
-              )}
+              <button className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold 
+              shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 
+              disabled:cursor-default"
+              disabled={!input && !selectedFile}
+              onClick={sendPost}
+              >
+                Tweet
+              </button>
+
             </div>
 
-            <button className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold 
-            shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 
-            disabled:cursor-default"
-            disabled={!input && !selectedFile}
-            // onClick={sendPost}
-            >
-              Tweet
-            </button>
-
-          </div>
+                )}
         </div>           
     </div>
   )
