ca89246565682adbc91b53e5a2fd65614043f17f
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-12 16:45:31 +0200

    feat: retriving post from database and displaying it / post buttons

--- twitter-clone/components/Feed.js
+++ twitter-clone/components/Feed.js
@@ -1,7 +1,41 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
 import Input from "./Input";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 export default function Feed() {
+
+  const [ posts, setPosts ] = useState([]);
+
+  // MESSY
+  // useEffect(() => {
+  //   const unsubscribe = onSnapshot(
+  //     query(collection(db, "posts"), orderBy("timestamp", "desc")),
+  //     (snapshot) => {
+  //       setPosts(snapshot.docs);
+  //     }
+  //   );
+
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [db]);
+
+  // CLEAN
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className="flex-grow border-l border-r border-gray-700 max-w-2xl 
     sm:ml-[73px] xl:ml-[370px]">
@@ -15,11 +49,11 @@ export default function Feed() {
 
       <Input />
 
-      {/* <div className="pb-72">
+      <div className="pb-72">
         {posts.map((post) => (
           <Post key={post.id} id={post.id} post={post.data()} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
