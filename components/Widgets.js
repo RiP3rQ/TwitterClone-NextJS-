a37cf5e90205cf32d4febcd4b4004df1bd028143
RiP3rQ <rafalpompa2000@gmail.com>
2022-10-17 00:29:39 +0200

    fixed production

--- components/Widgets.js
+++ components/Widgets.js
@@ -17,7 +17,7 @@ export default function Widgets({ trendingResults, followResults }) {
         </div>
 
         <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
-            <h4 className="font-bold text-xl px-4">What's happening</h4>
+            <h4 className="font-bold text-xl px-4">What&quot;s happening</h4>
                 {trendingResults.map((result, index) => (
                 <Trending key={index} result={result} />
                 ))}
